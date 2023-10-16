58b2d2662aec0f3052c5599cfdd73faf13aca55d
Pierre Morel <pmorel@linux.ibm.com>
2023-10-16 20:39:06 +0200

    CPU topology: extend with s390 specifics
    S390 adds two new SMP levels, drawers and books to the CPU
    topology.
    S390 CPUs have specific topology features like dedication and
    entitlement. These indicate to the guest information on host
    vCPU scheduling and help the guest make better scheduling decisions.
    
    Add the new levels to the relevant QAPI structs.
    Add all the supported topology levels, dedication and entitlement
    as properties to S390 CPUs.
    Create machine-common.json so we can later include it in
    machine-target.json also.
    
    Signed-off-by: Pierre Morel <pmorel@linux.ibm.com>
    Reviewed-by: Nina Schoetterl-Glausch <nsg@linux.ibm.com>
    Co-developed-by: Nina Schoetterl-Glausch <nsg@linux.ibm.com>
    Reviewed-by: Thomas Huth <thuth@redhat.com>
    Signed-off-by: Nina Schoetterl-Glausch <nsg@linux.ibm.com>
    Message-ID: <20231016183925.2384704-3-nsg@linux.ibm.com>
    Signed-off-by: Thomas Huth <thuth@redhat.com>

--- hw/s390x/s390-virtio-ccw.c
+++ hw/s390x/s390-virtio-ccw.c
@@ -738,6 +738,8 @@ static void ccw_machine_class_init(ObjectClass *oc, void *data)
     mc->no_sdcard = 1;
     mc->max_cpus = S390_MAX_CPUS;
     mc->has_hotpluggable_cpus = true;
+    mc->smp_props.books_supported = true;
+    mc->smp_props.drawers_supported = true;
     assert(!mc->get_hotplug_handler);
     mc->get_hotplug_handler = s390_get_hotplug_handler;
     mc->cpu_index_to_instance_props = s390_cpu_index_to_props;
@@ -835,6 +837,8 @@ static void ccw_machine_8_1_instance_options(MachineState *machine)
 
 static void ccw_machine_8_1_class_options(MachineClass *mc)
 {
+    mc->smp_props.drawers_supported = false;
+    mc->smp_props.books_supported = false;
 }
 DEFINE_CCW_MACHINE(8_1, "8.1", true);
 
