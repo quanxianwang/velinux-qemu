4a78f87ffd73cc930db33deaf22857b792517203
Xiaoyao Li <xiaoyao.li@intel.com>
2024-12-19 06:01:21 -0500

    i386/cpu: Track a X86CPUTopoInfo directly in CPUX86State
    The name of nr_modules/nr_dies are ambiguous and they mislead people.
    
    The purpose of them is to record and form the topology information. So
    just maintain a X86CPUTopoInfo member in CPUX86State instead. Then
    nr_modules and nr_dies can be dropped.
    
    As the benefit, x86 can switch to use information in
    CPUX86State::topo_info and get rid of the nr_cores and nr_threads in
    CPUState. This helps remove the dependency on qemu_init_vcpu(), so that
    x86 can get and use topology info earlier in x86_cpu_realizefn(); drop
    the comment that highlighted the depedency.
    
    Signed-off-by: Xiaoyao Li <xiaoyao.li@intel.com>
    Link: https://lore.kernel.org/r/20241219110125.1266461-7-xiaoyao.li@intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- target/i386/cpu.h
+++ target/i386/cpu.h
@@ -1899,10 +1899,7 @@ typedef struct CPUArchState {
 
     TPRAccess tpr_access_type;
 
-    unsigned nr_dies;
-
-    /* Number of modules within one die. */
-    unsigned nr_modules;
+    X86CPUTopoInfo topo_info;
 
     /* Bitmap of available CPU topology levels for this CPU. */
     DECLARE_BITMAP(avail_cpu_topo, CPU_TOPOLOGY_LEVEL__MAX);
