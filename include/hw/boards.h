6c340a23e7f9bd80deeabc15179b334c0c1d6ef7
Alireza Sanaee <alireza.sanaee@huawei.com>
2025-01-10 22:51:15 +0800

    i386/cpu: add has_caches flag to check smp_cache configuration
    Add has_caches flag to SMPCompatProps, which helps in avoiding
    extra checks for every single layer of caches in x86 (and ARM in
    future).
    
    Signed-off-by: Alireza Sanaee <alireza.sanaee@huawei.com>
    Signed-off-by: Zhao Liu <zhao1.liu@intel.com>
    Reviewed-by: Jonathan Cameron <Jonathan.Cameron@huawei.com>
    Reviewed-by: Michael S. Tsirkin <mst@redhat.com>
    Link: https://lore.kernel.org/r/20250110145115.1574345-6-zhao1.liu@intel.com
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- include/hw/boards.h
+++ include/hw/boards.h
@@ -147,6 +147,8 @@ typedef struct {
  * @modules_supported - whether modules are supported by the machine
  * @cache_supported - whether cache (l1d, l1i, l2 and l3) configuration are
  *                    supported by the machine
+ * @has_caches - whether cache properties are explicitly specified in the
+ *               user provided smp-cache configuration
  */
 typedef struct {
     bool prefer_sockets;
@@ -157,6 +159,7 @@ typedef struct {
     bool drawers_supported;
     bool modules_supported;
     bool cache_supported[CACHE_LEVEL_AND_TYPE__MAX];
+    bool has_caches;
 } SMPCompatProps;
 
 /**
